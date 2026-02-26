40bb72a2087ba2f8c5d3b0ab9684656ed9bb4d75
Vinicius Lopes <vlopes.ti@gmail.com>
2026-02-26 10:16:27 -0300

    altera a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 //Componente REACT (uma função js)
 function Home() {
-    return <h1>Juliana, eu amo você. Se você me ama, dá uma risadinha!</h1>
+    return <h1>Juliana, eu amo você. Se você me ama, dá uma risadinha! :) </h1>
 }
 
 export default Home; // sinaliza ao NEXT o que renderizar na página
